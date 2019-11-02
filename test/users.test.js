f92cd8b9691b8d6253f154da01628b046a03a6c6
Tauhidul Islam <tauhidul.bitspeck@gmail.com>
2019-11-03 01:34:57 +0600

    re structured node best API

--- test/users.test.js
+++
@@ -1,8 +0,0 @@
-const users = require('../api/routes/users');
-
-describe('users', () => {
-  it('something', () => {
-    const result = users.something(-1);
-    expect(result).toBe(1);
-  });
-});
