f23718aec8a7f959469cd5ff078bea3de6f914eb
Tauhidul Islam Tuhin <abdurshahin5@gmail.com>
2019-11-03 01:59:27 +0600

    Create keys.js

---
+++ server/src/config/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mongo: {
+    mongoURI:
+      'added you'r mongoDB ex: http://localhost:2773/project',
+    secretOrKey: 'secret'
+  },
+  secret: {
+    key: 'secret'
+  },
+  secretOrKey: 'secret'
+};
